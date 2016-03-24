c82f614556384ffc137e9b548287fab5c07b8ea7
masanom <logos@netplateau.jp>
2016-03-24 10:47:12 +0000

    after mentering 0324

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,7 +12,9 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = Amazon::Ecs.item_lookup(ARGV[0], { response_group: 'Large' })
+        response = Amazon::Ecs.item_lookup(@item.asin,
+                                          response_group: 'Midium',
+                                          country: 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
@@ -30,6 +32,7 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
+    current_user.have(@item) if params[:type] == "Have"
     
 
   end
