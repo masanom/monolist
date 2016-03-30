0514f9eb26222177d0e1e6b7ca69a59683438efa
masanom <logos@netplateau.jp>
2016-03-30 12:37:28 +0000

    Lesson8 last challnege

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,11 +1,13 @@
 class RankingController < ApplicationController
   def ranking_have
-    @items = Item.all.order("have DESC").limit(30)
-    #render ranking_have
-    
+    items = Have.group(:item_id).order('count_id desc').limit(10).count(:id) 
+    @ranking_have_items = items.transform_keys!{ |key| Item.find(key) }
+    render 'ranking_have' 
   end
   
   def ranking_want
+    @items = Item.all.order("type DESC").limit(10)
+    render 'ranking_want' 
   end
   
 end
