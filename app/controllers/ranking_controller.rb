6fd791c85a23611a21f9af75071921d8fad1170c
masanom <logos@netplateau.jp>
2016-04-01 05:36:15 +0000

    create ranking_have.html.erb

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,12 +1,13 @@
 class RankingController < ApplicationController
   def ranking_have
-    items = Have.group(:item_id).order('count_id desc').limit(10).count(:id) 
-    @ranking_have_items = items.transform_keys!{ |key| Item.find(key) }
+    @items = Have.group(:item_id).order('count_item_id desc').limit(10).count('item_id').keys 
+    @ranking_have_items = Item.find(@items).sort_by{|o| @items.index(o.id)}
     render 'ranking_have' 
   end
   
   def ranking_want
-    @items = Item.all.order("type DESC").limit(10)
+    items = Want.group(:item_id).order('count_id desc').limit(10).count(:id) 
+    @ranking_want_items = items.transform_keys!{ |key| Item.find(key) }
     render 'ranking_want' 
   end
   
